ad190a72ef41c7585b046bcee0ebd38cf18bdfda
JKodes <adrainford09@gmail.com>
2024-05-03 16:34:12 -0400

    extra

--- config/urls.py
+++ config/urls.py
@@ -16,14 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from django.conf import settings
 from django.conf.urls.static import static 
+from .views import SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('trip.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/signup/', SignupView.as_view(), name='signup'),  
 ]
 
 if settings.DEBUG:
