dc71cdfbc13fc0cf3361e8e0a0815d30f3d8dcae
JKodes <adrainford09@gmail.com>
2024-05-04 09:18:37 -0400

    change the view

--- trip/views.py
+++ trip/views.py
@@ -7,7 +7,17 @@ class HomeView(TemplateView):
     template_name = 'trip/index.html'
 
 def trips_list(request):
-    trips = Trip.objects.filter(owner=request.user)
+    if request.user.is_authenticated:
+        trips = Trip.objects.filter(owner=request.user)
+    else:
+        # Check if the user has visited the dashboard before
+        if 'visited_dashboard' in request.session:
+            # User has visited before, show the trips
+            trips = Trip.objects.filter(owner=request.session['visited_dashboard'])
+        else:
+            # User has not visited before, show no trips
+            trips = []
+
     context ={
         'trips' : trips
     }
