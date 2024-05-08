05e36a65e3b7058f7083b032119a86a2f3aa23d7
JKodes <adrainford09@gmail.com>
2024-05-08 10:54:12 -0400

    add update view and delete and create path

--- trip/urls.py
+++ trip/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from .views import HomeView, NoteCreateView, NoteDetailView, NoteListView, TripCreateView, TripDetailView, trips_list
+from .views import HomeView, NoteCreateView, NoteDeleteView, NoteDetailView, NoteListView, NoteUpdateView, TripCreateView, TripDetailView, trips_list
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('dashboard/', trips_list, name='trip-list'),
     path('dashboard/note/', NoteListView.as_view(), name='note-list'),
-    path('dashboard/note/create/', NoteCreateView.as_view(), name='note-create'),
+    path('dashboard/note/create/', NoteCreateView.as_view(), name='note-'),
     path('dashboard/trip/create', TripCreateView.as_view(), name='trip-create'),
     path('dashboard/trip/<int:pk>/', TripDetailView.as_view(), name='trip-detail'),
     path('dashboard/note/<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
-    
+    path('dashboard/note/<int:pk>/upate/', NoteUpdateView.as_view(), name='note-update'),
+    path('dashboard/note/<int:pk>/delete/', NoteDeleteView.as_view(), name='note-delete'),
 ]
